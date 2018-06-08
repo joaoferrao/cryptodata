71c251d0e877ed35208200c45a4fe0e9ffe54660
João Ferrão <jrcferrao@gmail.com>
2018-06-08 19:53:14 +0100

    pyspark notebook with starter df.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ from typing import List
 
 
 # Setup which symbols you want to download data from
-CRYPTOS: List = ['BTC', 'ETH', 'EOS', 'LTC']
+# CRYPTOS: List = ['BTC', 'ETH', 'EOS', 'LTC']
+CRYPTOS: List = ['EOS']
 
 
 def define_rest_url(symbol: str, currency: str, todate: int) -> str:
@@ -52,13 +53,14 @@ def query_symbol(symbol: str, currency: str, todate: int=None):
     df = response_to_dataframe(r)
     df.query('close != 0.0', inplace=True)
     while next_todate != 0:
+        print("Still fetching results for {}".format(symbol))
         url = define_rest_url(symbol=symbol, currency=currency, todate=next_todate)
         r, next_todate = get_batch_response(url)
         df = df.append(response_to_dataframe(r), ignore_index=True)
 
     df = convert_col_int_to_dt(df, ["time"])
     df.sort_values(by=["time"], inplace=True)
-    df['Symbol'] = symbol
+    df['symbol'] = symbol
     return df.query('close != 0.0')
 
 
@@ -72,10 +74,12 @@ def download_data(list_cryptos: List[str], currency: str, todate: int=None):
 
     fp = os.getcwd()
     par_dir = os.path.dirname(fp)
-    abs_path = os.path.join(fp, 'data', 'data.csv')
+    abs_path = os.path.join(par_dir, 'data', 'data_raw.csv')
+    print("Saving file to {}".format(abs_path))
     df.to_csv(path_or_buf=abs_path, encoding='utf-8', index=False,)
 
 
 if __name__ == '__main__':
     print("Starting Download")
     download_data(CRYPTOS, currency='USD')
+    print("Done")
